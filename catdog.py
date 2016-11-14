352ae0b468cfecd0ce1caed9a569af13dc1d8d6e
Fausto G. Tommasi <mmasito123@yahoo.com>
2016-11-14 17:05:45 -0600

    Added more functionality to graph elements. Added unit tests

--- catdog.py
+++ catdog.py
@@ -1,10 +1,18 @@
 from graph import *
 
+
+source = Vertex();
+sink = Vertex();
 def findConflicts(c,d):
   for v1 in c:
     for v2 in d:
-      if v1.choice == v2.choice:
+      if v1.getChoice() == v2.getChoice():
         createEdge(v1,v2)
+
+def createEdge(v1, v2):
+  e = Edge(v1,v2,1)
+  v1.addEdge(e)
+  v2.addEdge(e)
 
 #parse input
 entries = input()
@@ -15,9 +23,14 @@ for i in xrange(entries):
   for j in xrange(int(v)):
     stay,leave= raw_input().split()
     #print "stay",stay,"leave",leave
+    v = Vertex()
+    v.setChoice(stay + " " + leave)
     if stay[0] == "C":
-      v = Vertex()
       catlovers.append(v)
+      createEdge(source,v)
     elif stay[0] == "D":
       doglovers.append(v)
+      createEdge(v,sink)
   findConflicts(catlovers,doglovers)
+  print n for n in source.getNeighbors()
+  print n for n in sink.getNeighbors()
--- graph.py
+++ graph.py
@@ -1,6 +1,50 @@
+#! /usr/bin/env python
 class Vertex:
-  def __init__(self):
-    self._edges = [] 
+  def __init__(self,choice = ""):
+    self._edges = []
+    self._choice = choice
+  def addEdge(self,e):
+    self._edges.append(e)
+  
+  def getEdges(self):
+    return self._edges
+  
+  def viewNeighbors(self):
+    neigh = []
+    for edge in self._edges:
+      if not edge.getU() is not self:
+        if edge.getU() not in neigh:
+          neigh.append(edge.getU())
+      
+      elif edge.getV() is not self:
+        if edge.getV() not in neigh:
+          neigh.append(edge.getV())
+    
+    return neigh
+
+  def getChoice(self):
+    return self._choice
+  
+  def setChoice(self,c):
+    self._choice = c
+
+  def __str__(self):
+    result = self._choice
+    for edge in self._edges:
+      if edge.getU() is not self and edge.getU() is not None :
+        result += str(edge)
+        result += str(edge.getU().getChoice())+"\n"
+      
+      elif edge.getV() is not self and edge.getU() is not None:
+        result += str(edge)
+        result += str(edge.getV().getChoice())+"\n"
+    
+
+    return result
+
+  #def __repr__(self):
+   # return
+
 class Edge:
   
   def __init__(self,u=None,v=None,c=1):
@@ -8,26 +52,51 @@ class Edge:
     self._v = v
     self._capacity = c
 
-  def getU():
+  def getU(self):
     return self._u
   
-  def getV():
+  def getV(self):
     return self._v
 
-  def getCap():
+  def getCap(self):
     return self._capacity
 
-  def setU(u):
+  def setU(self,u):
     self._u = u
   
-  def setV(v):
+  def setV(self,v):
     self._v = v
 
-  def setCap(c):
+  def setCap(self,c):
     self._capacity=c
 
+  def __str__(self):
+    return "--%s-->"%(self._capacity)
+
 if __name__ == "__main__":
-  v = Vertex()
+  v = Vertex("V")
   e = Edge()
+  v.addEdge(e)
+  v.getEdges()
+  v2 = Vertex("V1")
+  e2 = Edge(v,v2,1,)
+#VERTEX
+  v.addEdge(e2)
+  v2.addEdge(e2)
+  v.getEdges()
+  v2.getEdges()
+  v.viewNeighbors()
+  v.getChoice()
+  v.setChoice("NEW V")
+  print v.__str__()
+#END VERTEX
 
-
+#EDGE
+  e.getU()
+  e.getV()
+  e.getCap()
+  e.setU(v)
+  e.setV(v2)
+  e.setCap(10)
+  print e2.__str__()
+#ENDEDGE
