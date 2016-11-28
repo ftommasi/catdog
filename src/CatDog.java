8d82b0a8d059cfde26d71312c9039af9ca8c4dd2
Fausto Tommasi <Fausto Tommasi>
2016-11-28 12:45:37 -0600

    Java implementation

---
+++ src/CatDog.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+
+public class CatDog {
+	public static void main(String[] args) throws IOException{
+		
+		int n = System.in.read();
+		for(int ITERATIONS=0;ITERATIONS<n;ITERATIONS++){
+			
+			int c,d,v;
+			c = System.in.read();
+			d = System.in.read();
+			v = System.in.read();
+			for(int voters =0; voters<v;voters++){
+				//parse input here
+				
+			}			
+		}
+	}
+}
